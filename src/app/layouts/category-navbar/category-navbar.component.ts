18c033d84011493c0a310032d037e36d97d716b6
Nishanthi <118036003+Nisha091999@users.noreply.github.com>
2024-05-28 08:32:45 +0530

    Add files via upload

---
+++ src/app/layouts/category-navbar/category-navbar.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { CategoriesService } from 'src/app/services/categories.service';
+
+@Component({
+  selector: 'app-category-navbar',
+  templateUrl: './category-navbar.component.html',
+  styleUrls: ['./category-navbar.component.css']
+})
+export class CategoryNavbarComponent implements OnInit{
+
+  categoryArray: any;
+
+  constructor( private categoryService: CategoriesService) { }
+  
+  ngOnInit(): void {
+
+    this.categoryService.loadData().subscribe((val: any) => {
+      this.categoryArray=val;
+    })
+
+  }
+
+}
